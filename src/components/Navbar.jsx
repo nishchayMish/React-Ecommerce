6c9f0182dfdab9767b53a2ace379606ab949cbe2
nishchayMish <119095279+nishchayMish@users.noreply.github.com>
2025-02-27 00:50:37 +0530

    added input functionality and added full collection page along with full functionality like related images and related products

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,11 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import {assets} from '../assets/frontend_assets/assets'
 import { Link, NavLink } from 'react-router-dom'
+import { ShopContext } from '../context/ShopContext'
 
 const Navbar = () => {
   const[sidebarVisible, setSidebarVisible] = useState(false)
+  const { setShowSearch } = useContext(ShopContext);
   return (
     <div className='flex justify-between items-center py-5 font-medium'>
       <Link to='/'>
@@ -32,7 +34,7 @@ const Navbar = () => {
       </ul> 
 
       <div className='flex items-center gap-6'>
-          <img src={assets.search_icon} className='w-5 cursor-pointer'/>
+          <img onClick={()=>setShowSearch(true)} src={assets.search_icon} className='w-5 cursor-pointer'/>
           <div className='group relative'>
             <img src={assets.profile_icon} className='w-5 cursor-pointer'/>
             <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -1,12 +1,15 @@
-import React, { createContext } from 'react'
+import React, { createContext, useState } from 'react'
 import { products } from '../assets/frontend_assets/assets';
 export const ShopContext = createContext(null);
 
 const ShopContextProvider = ({children}) => {
     const currency = '$';
     const delivery_fee = 10;
+    const [search, setSearch] = useState('');
+    const [showSearch, setShowSearch] = useState(false);
     const value = {
-        products, currency, delivery_fee
+        products, currency, delivery_fee,search, 
+        setSearch, showSearch, setShowSearch
     }
 
     return(
