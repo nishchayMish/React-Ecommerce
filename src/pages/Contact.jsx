720198f7011eb14b3136a6d2d769bb3b53fbb8e0
nishchayMish <119095279+nishchayMish@users.noreply.github.com>
2025-02-28 17:21:16 +0530

    full responsive contact page and about page added

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import Title from '../components/Title'
+import NewsLetterBox from '../components/NewsLetterBox'
+import { assets } from '../assets/frontend_assets/assets'
+
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div>
+      <div className='text-center text-2xl pt-10 border-t '>
+        <Title text1={'CONTACT'} text2={'US'}/>
+      </div>
+      <div className='my-10 flex flex-col justify-center md:flex-row gap-10 mb-20'>
+        <img src={assets.contact_img} className='w-full md:max-w-[480px]' />
+        <div className='flex flex-col justify-center items-start gap-6'>
+          <p className='font-semibold text-xl text-gray-600'>Our Store</p>
+          <p className='text-gray-500'>Hermes Headquarters <br/> 123 Fashion Avenue, Rosewood District <br/> New Delhi, India</p>
+          <p className='text-gray-500'>Tel: +91 98765 43210 0 <br/> Email: admin@Hermes.com</p>
+          <p className='font-semibold text-xl text-gray-600'>Carres at Hermes</p>
+          <p className='text-gray-500'>Learn more about our teams and job openings.</p>
+          <button className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500 hover:cursor-pointer'>Explore Jobs</button>
+        </div>
+      </div>
+      <NewsLetterBox/>
+    </div>
   )
 }
 
